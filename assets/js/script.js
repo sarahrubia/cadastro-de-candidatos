9cfcd0496f04788502f47cde79da6c33c85f7aeb
sarahrubia <sarahrubia22@gmail.com>
2022-04-07 17:04:45 -0300

    teste

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ function desenhaTabela() {
                     </td>
                     <td>
                         <button onclick="deletarAlunos(${dados});">Excluir</button>
-                        <a href="projeto-guiado-nta/src/form.html?dadosAlunos=${dados}">Alterar</a>
+                        <a href="./src/form.html?dadosAlunos=${dados}">Alterar</a>
                     </td>
                 </tr>
                 `
